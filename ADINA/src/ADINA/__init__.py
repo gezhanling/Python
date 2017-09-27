34a24d1a1668c8a8be627d54d90deff8a2add556
gezhanling <1138440880@qq.com>
2017-09-27 21:09:11 +0800

    版本1.3：完成了不用设置初始速度就能替换速度的问题，完成了打开文件夹的功能；待完成功能为：打开特定的TXT文件，并得到其中的信息。

--- ADINA/src/ADINA/__init__.py
+++ ADINA/src/ADINA/__init__.py
@@ -11,12 +11,15 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.calc_tax_button.clicked.connect(self.CalculateTax)
+        self.volt_seek.clicked.connect(self.file_dialog)
         self.go_to_exe.clicked.connect(self.GoToExe)
-        
+
     def CalculateTax(self):
         velocity = str(self.price_box.toPlainText())
+        f = open("D:\C-example05f.in","r")
+        f.seek(9536,1)
         global first
-        first = "VX=3.33"
+        first = str(f.read(7))
         if self.Judge() == 1:
             with open("D:\C-example05f.in","r") as f:
                 lines = f.readlines()
@@ -24,7 +27,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
                 for line in lines:
                     if first in line:
                         line = line.replace(first,velocity)
-                f_w.write(line)
+                    f_w.write(line)
                 global prior
                 prior = velocity
         else:
@@ -34,12 +37,15 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
                 for line in lines:
                     if prior in line:
                         line = line.replace(prior,velocity)
-                f_w.write(line)
+                    f_w.write(line)
         prior = velocity    
                                 
     def GoToExe(self):
-        #os.system(r'"F:\Program Files (x86)\ADINA90\x32\aui.exe"')
-        os.system(r'"C:\Users\zhanling_ge\Desktop\adina.bat"')
+        os.system(r'"D:\adina.bat"')
+    def file_dialog(self):
+        file_name = QtGui.QFileDialog.getOpenFileName(self,"open file dialog","C:\Users\zhanling_ge\Desktop","Txt files(*.txt)") 
+        file = open(file_name,'r').read()
+        self.textEdit.setText(file)
     def Judge(self):
         with open("D:\C-example05f.in","r") as f:
             lines = f.readlines()    
